a811bd7ceefe68c30f2f3519e3660849bde6b16d
Sabrina Godoy <sabrygodoy1@gmail.com>
2024-03-07 10:50:33 -0300

    primeros pasos en react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { Input } from './components/Input';
+
+function App() {
+  return (
+    <>
+      <h1>Holis</h1>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      </>
+  );
+}
+
+export default App
