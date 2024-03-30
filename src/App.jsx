f26cb82c483227ade487acc63c9f65624bb8f138
Sabrina Godoy <sabrygodoy1@gmail.com>
2024-03-30 12:25:31 -0300

    uniendo la API

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,41 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { ItemListContainer } from "./components/ItemListContainer";
 import { NavBar } from "./components/NavBar";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [articulos, setArticulos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const url = "https://6608248aa2a5dd477b140db6.mockapi.io/articles";
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setArticulos(data))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <>
-      <NavBar />
-      <ItemListContainer greeting="Proximamente, los productos aquí..." />
+      {loading ? (
+        <div>Loading</div>
+      ) : (
+        <div>
+          {articulos.map((articulo) => (
+            <>
+              <h2>{articulo.name}</h2>
+              <h4>ID: {articulo.id}</h4>
+            </>
+          ))}
+        </div>
+      )}
     </>
   );
+  // <>
+  //   <NavBar />
+  //   <ItemListContainer greeting="Proximamente, los productos aquí..." />
+  // </>
 }
 
 export default App;
